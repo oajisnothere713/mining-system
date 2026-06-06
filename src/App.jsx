27264e1cae937004701b856ac2780ee0e8c7f33c
oajisnothere713 <borwankaroaj@gmail.com>
2026-06-06 12:04:48 +0530

    initial prototype deployement

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import FieldOpsIntelligence from './FieldOpsIntelligence';
+
+function App() {
+  return (
+    <div className="App">
+      <FieldOpsIntelligence />
+    </div>
+  );
+}
+
+export default App;
